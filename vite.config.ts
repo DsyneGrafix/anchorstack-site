ab239a6086b5648f2aa9cec13784f5981e22d13b
Ricky Jarnagin <28316323+DsyneGrafix@users.noreply.github.com>
2025-07-24 14:27:27 -0500

    fix: clean up vite.config.ts and enable path aliasing

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import * as path from 'path' // ✅ FIXED
-import { fileURLToPath } from 'url' // ✅ FIXED
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { Button } from '@/components/ui/Button';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
@@ -13,5 +14,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
 
