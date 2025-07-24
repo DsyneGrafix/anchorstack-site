34ea7f7e6891c3fcd4fc87af4a0dcdf0066ea792
Ricky Jarnagin <28316323+DsyneGrafix@users.noreply.github.com>
2025-07-24 13:07:12 -0500

    chore: trigger Netlify cache bust

--- src/components/QuickNotes.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const QuickNotes = () => {
-    return _jsx("div", { children: "\uD83D\uDCDD Quick Notes placeholder" });
-};
-export default QuickNotes; // ✅ Required!
